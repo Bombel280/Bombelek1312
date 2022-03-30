3a9ca97fef714d661094a16445af6db69d51a422
Bombel280 <100341557+Bombel280@users.noreply.github.com>
2022-03-30 08:55:07 +0200

    Add files via upload

---
+++ nww i nwd/nww i nwd.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+    int countTime;
+    int lp = 0;
+    int countl = 0;
+    int nextl = 2;
+
+    cout << "Podaj ile chcesz wygenerować liczb: ";
+    cin >> countTime;
+
+    while (lp < countTime)
+
+    {
+        for (int d = 2; d <= nextl - 1; d++)
+            if (nextl % d == 0) { countl++; }
+
+        if (countl == 0) { cout << " " << nextl; lp++; }
+        nextl++;
+        countl = 0;
+
+    }
+
+}
